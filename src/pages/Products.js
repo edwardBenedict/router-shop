96feb015f886652fcc50be86dcb62af8c523c765
edwardBenedict <edwardbenedict92@gmail.com>
2022-04-02 15:30:50 +0200

    after api

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import styles from "./Product.module.css";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
+  return (
+    <div>
+      <h2>Product Page</h2>
+      <div>
+        {products.map((each) => (
+          <div key={each.id}>
+            <h6>{each.category}</h6>
+            <p>{each.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
